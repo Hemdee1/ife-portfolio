2b5c7e1c3b45e95839622930ab0be309c0066877
hemdee <sanusi5110@gmail.com>
2022-11-17 22:39:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Satoshi: "Satoshi, sans-serif",
+        Cabinet: "Cabinet Grotesk, sans-serif",
+      },
+      colors: {
+        background: "#030E02",
+        primary: "#45CA2F",
+        text: "#FFEFFD",
+      },
+    },
+  },
+  plugins: [],
+};
