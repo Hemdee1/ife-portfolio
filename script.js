6da06ada82e30731f035e79811780d57f773543c
hemdee <sanusi5110@gmail.com>
2022-11-19 21:36:03 +0100

    fix cursor issue

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ window.addEventListener("mousemove", (e) => {
   const y = e.clientY;
 
   setTimeout(() => {
-    innerCursor.style.left = x + 11 + "px";
-    innerCursor.style.top = y + 11 + "px";
+    innerCursor.style.left = x - 4 + "px";
+    innerCursor.style.top = y - 4 + "px";
   }, 70);
 
   setTimeout(() => {
-    outerCursor.style.left = x + "px";
-    outerCursor.style.top = y + "px";
+    outerCursor.style.left = x - 15 + "px";
+    outerCursor.style.top = y - 15 + "px";
   }, 140);
 });
